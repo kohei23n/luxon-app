c63f7bbb8cdbfe7138636c4a749d255eaa62566b
kohei23n <kohei.s.n@gmail.com>
2023-10-08 22:03:33 +0900

    update: 追加マイグレーション完了

---
+++ src/database/migrations/2023_10_08_124241_create_luxon_mst_company_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('luxon_mst_company', function (Blueprint $table) {
+            $table->increments('mco_company_id')->comment('会社ID');
+            $table->unsignedInteger('mco_industry_id')->comment('業界ID');
+            $table->string('mco_company_name')->comment('会社名');
+            $table->string('mco_company_logo_s3_url')->comment('会社ロゴS3 URL');
+            $table->text('mco_company_overview')->comment('会社概要');
+            $table->char('mco_delete_flag', 1)->default('0')->comment('削除フラグ');
+            $table->dateTime('mco_deletion_datetime')->nullable()->comment('削除日時');
+            $table->dateTime('mco_registration_datetime')->comment('登録日時');
+            $table->dateTime('mco_update_datetime')->comment('更新日時');
+            $table->timestamp('mco_update_timestamp')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comment('システム更新日時');
+
+            $table->foreign('mco_industry_id')->references('min_industry_id')->on('luxon_mst_industry');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('luxon_mst_company');
+        Schema::enableForeignKeyConstraints();
+    }
+};
---
+++ src/database/migrations/2023_10_08_124618_create_luxon_mst_selection_detail_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('luxon_mst_selection_detail', function (Blueprint $table) {
+            $table->increments('msd_selection_detail_id')->comment('選考詳細ID');
+            $table->unsignedInteger('msd_industry_id')->comment('業界ID');
+            $table->unsignedInteger('msd_company_id')->nullable()->comment('会社ID');
+            $table->string('msd_selection_phase_name')->comment('選考段階名');
+            $table->string('msd_selection_detail')->nullable()->comment('選考詳細');
+            $table->string('msd_selection_materials_URL')->nullable()->comment('選考資料URL');
+            $table->char('msd_delete_flag', 1)->default('0')->comment('削除フラグ');
+            $table->dateTime('msd_deletion_datetime')->nullable()->comment('削除日時');
+            $table->dateTime('msd_registration_datetime')->comment('登録日時');
+            $table->dateTime('msd_update_datetime')->comment('更新日時');
+            $table->timestamp('msd_update_timestamp')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comment('システム更新日時');
+
+            $table->foreign('msd_industry_id')->references('min_industry_id')->on('luxon_mst_industry');
+            $table->foreign('msd_company_id')->references('mco_company_id')->on('luxon_mst_company');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('luxon_mst_selection_detail');
+    }
+};
