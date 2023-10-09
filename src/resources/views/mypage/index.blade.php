99e462d6cd369b86f41a80f5abfbea51ea1df355
kohei23n <kohei.s.n@gmail.com>
2023-10-09 14:33:41 +0900

    update: マイページ一通り完成

--- src/resources/views/mypage/index.blade.php
+++ src/resources/views/mypage/index.blade.php
@@ -1,21 +1,21 @@
 <x-app-layout>
-  <x-slot name="header">
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          {{ __('Top') }}
-      </h2>
-  </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Top') }}
+        </h2>
+    </x-slot>
 
-  <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <p>ようこそ、{{ $user->mus_user_last_name }}{{ $user->mus_user_first_name }}さん</p>
-              <div class="p-6 text-gray-900">
-                  <a href="{{ route('mypage.planIndex') }}">プラン</a>
-                  <a href="{{ route('mypage.selectionIndex') }}">選考情報</a>
-                  <a href="">お気に入り</a>
-                  <a href="">お知らせ</a>
-              </div>
-          </div>
-      </div>
-  </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <p>ようこそ、{{ $user->mus_user_last_name }}{{ $user->mus_user_first_name }}さん</p>
+                <div class="p-6 text-gray-900">
+                    <a href="{{ route('mypage.planIndex') }}">プラン</a>
+                    <a href="{{ route('mypage.selectionIndex') }}">選考情報</a>
+                    <a href="">お気に入り</a>
+                    <a href="">お知らせ</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
--- src/routes/web.php
+++ src/routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\IndexController;
 // 業界研究
 use App\Http\Controllers\Research\IndexController as ResearchIndex;
 
+// 予約
+use App\Http\Controllers\Reserve\Interview\IndexController as ReserveInterviewIndex;
+
 // マイページ
 // トップ
 use App\Http\Controllers\MyPage\IndexController as MyPageIndex;
@@ -18,12 +21,6 @@ use App\Http\Controllers\MyPage\Plan\Profile\DeleteController as MyPagePlanProfi
 use App\Http\Controllers\MyPage\Selection\IndexController as MyPageSelectionIndex;
 use App\Http\Controllers\MyPage\Selection\CreateController as MyPageSelectionCreate;
 use App\Http\Controllers\MyPage\Selection\UpdateController as MyPageSelectionUpdate;
-// 個人情報
-use App\Http\Controllers\Profile\UpdateController as ProfileUpdate;
-
-
-// 予約
-use App\Http\Controllers\Reserve\IndexController as ReserveIndex;
 
 use Illuminate\Support\Facades\Route;
 
@@ -51,8 +48,17 @@ Route::middleware('auth')->group(function () {
 
 // マイページ
 Route::middleware('auth')->group(function () {
-    // トップ
+
+    // 予約：トップ
+    Route::get('/reserve', function () {
+        return view('reserve.index');
+    })->name('reserve.index');
+    // 予約：面談
+    Route::get('/reserve/interview', ReserveInterviewIndex::class)->name('reserve.interviewIndex');
+
+    // マイページトップ
     Route::get('/mypage', MyPageIndex::class)->name('mypage.index');
+
     // プラン
     Route::get('/mypage/plan', MyPagePlanIndex::class)->name('mypage.planIndex');
     // プラン：個人情報
@@ -60,16 +66,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/mypage/plan/pi/edit', [MyPagePlanProfileUpdate::class, 'update'])->name('mypage.plan.profileUpdate');
     // ここは後で直したい
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
     // 選考情報
     Route::get('/mypage/selection', MyPageSelectionIndex::class)->name('mypage.selectionIndex');
     Route::get('/mypage/selection/add', [MyPageSelectionCreate::class, 'add'])->name('mypage.selectionAdd');
     Route::post('/mypage/selection/add', [MyPageSelectionCreate::class, 'create'])->name('mypage.selectionCreate');
     Route::get('/mypage/selection/edit/{id}', [MyPageSelectionUpdate::class, 'edit'])->name('mypage.selectionEdit');
     Route::patch('/mypage/selection/edit/{id}', [MyPageSelectionUpdate::class, 'update'])->name('mypage.selectionUpdate');
-    // 選考
 });
 
-// 予約
-Route::get('/reserve', ReserveIndex::class)->name('reserve.index');
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
