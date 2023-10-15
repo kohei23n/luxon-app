9d026e7595d1538ea7d7c1752bb6fa18fc02bacc
RinKuroki <kuroki.rin@d-publishing.jp>
2023-10-15 13:26:10 +0900

    ログイン画面css完成

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,21 +1,29 @@
-<x-guest-layout>
-    <!-- Session Status -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+</head>
+<body>
+    <x-guest-layout>
+    <h1>Tachyon</h1>
+        <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
+    <h2>Mentee Sign In</h2>
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="mus_email_address" :value="__('Email')" />
-            <x-text-input id="mus_email_address" class="block mt-1 w-full" type="email" name="mus_email_address" :value="old('mus_email_address')" required autofocus autocomplete="username" />
+        <div class="mt-4">
+            <x-input-label for="mus_email_address" style="color:white;" :value="__('Email')" />
+            <x-text-input style="margin-top: 10px; background-color: white;" id="mus_email_address" class="block mt-1 w-full" type="email" name="mus_email_address" :value="old('mus_email_address')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('mus_email_address')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="mus_user_password" :value="__('Password')" />
-            <x-text-input id="mus_user_password" class="block mt-1 w-full"
+            <x-input-label for="mus_user_password" style="color:white;" :value="__('Password')" />
+            <x-text-input style="margin-top: 10px; background-color: white;" id="mus_user_password" class="block mt-1 w-full"
                             type="password"
                             name="mus_user_password"
                             required autocomplete="current-password" />
@@ -27,20 +35,21 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <span class="ml-2 text-sm text-gray-600" style="color:white;">{{ __('Remember me') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}"style="color:white;">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
-            <x-primary-button class="ml-3">
+            <x-primary-button style="color: blue;;" class="ml-3">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+    </x-guest-layout>
+</body>
