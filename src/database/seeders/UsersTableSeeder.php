ede584ca8052005bacb0fa312e2804753d89dc09
kohei23n <kohei.s.n@gmail.com>
2023-11-08 13:25:05 +0900

    update: database records

--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class UsersTableSeeder extends Seeder
@@ -53,6 +54,41 @@ class UsersTableSeeder extends Seeder
             'mus_user_first_name' => '三郎',
             'mus_user_last_name' => 'ユーザー',
             'mus_dedicated_mentor_id' => 2,
+        ]);
+        User::create([
+            'mus_email_address' => 'yhara1189@icloud.com',
+            'mus_user_password' => Hash::make('yhara1189@icloud.com'),
+            'mus_user_first_name' => 'よし',
+            'mus_user_last_name' => 'はら',
+            'mus_dedicated_mentor_id' => 1,
+        ]);
+        User::create([
+            'mus_email_address' => 'kinoshita.kaisei.465@s.kyushu-u.ac.jp',
+            'mus_user_password' => Hash::make('kaisei0419'),
+            'mus_user_first_name' => '海征',
+            'mus_user_last_name' => '木下',
+            'mus_dedicated_mentor_id' => 1,
+        ]);
+        User::create([
+            'mus_email_address' => 'nakajimaaa0725@gmail.com',
+            'mus_user_password' => Hash::make('36474738'),
+            'mus_user_first_name' => '将',
+            'mus_user_last_name' => '中島',
+            'mus_dedicated_mentor_id' => 1,
+        ]);
+        User::create([
+            'mus_email_address' => 'soh336699@gmail.com',
+            'mus_user_password' => Hash::make('Qwer1234'),
+            'mus_user_first_name' => '壮甫',
+            'mus_user_last_name' => '今村',
+            'mus_dedicated_mentor_id' => 1,
+        ]);
+        User::create([
+            'mus_email_address' => 'daoxiayoudeng@gmail.com',
+            'mus_user_password' => Hash::make('Yuppon0510'),
+            'mus_user_first_name' => '道下',
+            'mus_user_last_name' => '悠登',
+            'mus_dedicated_mentor_id' => 1,
         ]);
     }
 }
