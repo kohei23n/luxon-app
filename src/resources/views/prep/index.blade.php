b63a9169e4930269b793d97256ae319a68d7b867
kohei23n <kohei.s.n@gmail.com>
2023-11-18 22:47:17 +0900

    update: 不要なクラスの削除

--- src/resources/views/prep/index.blade.php
+++ src/resources/views/prep/index.blade.php
@@ -8,20 +8,18 @@
             {{ __('選考対策トップ') }}
         </h2>
     </x-slot>
-    <div class="reserving-wrapper">
-        <div class="reserve-container">
-            <div class="reserving-list">
-                <div class="reserving-box" onclick="window.location.href = '{{ route('prep.caseAdd') }}';"
-                    style="cursor: pointer;">
-                    <a href="{{ route('prep.caseAdd') }}">ケース添削</a>
-                </div>
+    <div class="reserve-container">
+        <div class="reserving-list">
+            <div class="reserving-box" onclick="window.location.href = '{{ route('prep.caseAdd') }}';"
+                style="cursor: pointer;">
+                <a href="{{ route('prep.caseAdd') }}">ケース添削</a>
+            </div>
 
-                <div class="reserving-box">
-                    <a href="">〜準備中〜</a><br>
-                </div>
-                <div class="reserving-box">
-                    <a href="">〜準備中〜</a><br>
-                </div>
+            <div class="reserving-box">
+                <a href="">〜準備中〜</a><br>
+            </div>
+            <div class="reserving-box">
+                <a href="">〜準備中〜</a><br>
             </div>
         </div>
     </div>
