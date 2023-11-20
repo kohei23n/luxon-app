af55a45cfd695e15d88200f5646b87e87cc485ae
kohei23n <kohei.s.n@gmail.com>
2023-11-20 23:44:51 +0900

    update: seeder更新

--- src/database/seeders/UserDetailTableSeeder.php
+++ src/database/seeders/UserDetailTableSeeder.php
@@ -14,6 +14,18 @@ class UserDetailTableSeeder extends Seeder
     public function run(): void
     {
         UserDetail::create([
+            'tud_user_id' => 2,
+            'tud_current_university' => 'なし',
+            'tud_first_industry_preference' => 'なし',
+            'tud_second_industry_preference' => 'なし',
+            'tud_service_plan_id' => 1,
+            'tud_event_attendance_remaining' => 0,
+            'tud_interview_count_remaining' => 0,
+            'tud_case_study_count_remaining' => 0,
+            'tud_es_count_remaining' => 0,
+        ]);
+
+        UserDetail::create([
             'tud_user_id' => 4,
             'tud_current_university' => '東京大学',
             'tud_first_industry_preference' => 'IT',
