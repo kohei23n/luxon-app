ef0050969e6bd4b1b27b1481e9b0836a30847e64
kohei23n <kohei.s.n@gmail.com>
2023-12-03 13:32:48 +0900

    update: デバッグ

--- src/app/Http/Controllers/Admin/Mentee/IndexController.php
+++ src/app/Http/Controllers/Admin/Mentee/IndexController.php
@@ -12,11 +12,16 @@ class IndexController extends Controller
         //選考情報表示
         $users = User::with(['userDetail', 'dedicatedMentor'])->where('mus_is_admin', 0)->where('mus_is_mentor', 0)->get();
 
+        $user = User::with(['userDetail', 'dedicatedMentor'])
+            ->where('mus_is_admin', 0)
+            ->where('mus_is_mentor', 0)
+            ->find(170);
+
+        // userDetailをdd()で出力
+        dd($user->userDetail);
+
         // 専属メンターの名前を取得
         $users->each(function ($user) {
-
-            dd($user->userDetail);
-            
             $mentor = User::with('mentorProfile')
                 ->where('mus_user_id', $user->mus_dedicated_mentor_id)
                 ->first();
